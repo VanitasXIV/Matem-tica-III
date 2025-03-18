c7e3e1d590578c6e452962f08c5b889801e22fbf
Ivan <ivangonzalogomez@gmail.com>
2025-03-18 17:15:59 -0300

    Guia 2 ejercicio 01

--- Guia02/ejercicio01.py
+++ Guia02/ejercicio01.py
@@ -1,9 +1,16 @@
 from sympy import *
 
+
+#Graficar la función f(x)=3x^2 +1
 x = symbols('x')
 f = (3*x**2) +1
 plot(f)
 
+# y su derivada
 dx_f = diff(f)
 print(dx_f)
 plot(dx_f)
+
+#¿Cuál es la pendiente en x=3?
+print(dx_f.subs(x,3))
+
